61d90e369ffe3190ca25630ad183a65e68d445a4
Keshavm2 <keshavmaharaj45@gmail.com>
2026-04-05 02:06:09 -0400

    Ship and Movement

--- app/static/js/asteroids.js
+++ app/static/js/asteroids.js
@@ -7,6 +7,63 @@ canvas.height = window.innerHeight;
 c.fillStyle = "black";
 c.fillRect(0, 0, canvas.width, canvas.height);
 
+//Creating a spaceship
+class SpaceShip {
+    constructor({position, velocity}){
+        this.position = position;
+        this.velocity = velocity;
+        this.rotation = 0;
+    }
+
+    draw(){
+
+        // Rotates the ship 
+        c.save();
+        c.translate(this.position.x, this.position.y);
+        c.rotate(this.rotation);
+        c.translate(-this.position.x, -this.position.y);
+
+        c.beginPath();
+        c.moveTo(this.position.x + 30, this.position.y);
+        c.lineTo(this.position.x - 10, this.position.y - 10);
+        c.lineTo(this.position.x - 10, this.position.y + 10);
+        c.closePath();
+        c.strokeStyle = "white";
+        c.stroke();
+        c.restore();
+        }
+
+    // Update spaceship position every frame
+    update(){
+        this.draw();
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+
+        }
+
+    }
+
+const spaceship = new SpaceShip({
+
+    position:{x: canvas.width / 2, y: canvas.height / 2}, 
+    velocity:{x: 0, y: 0},
+
+    });
+
+    const keys ={
+      w: {
+        pressed: false
+      },
+      a: {
+        pressed: false
+      },
+      d: {
+        pressed: false
+      },
+    };
+
+
+//Creating asteroids
 class Asteroid {
     constructor({position, velocity, radius}) {
         this.position = position;
@@ -29,8 +86,86 @@ class Asteroid {
     }
 }
 
+const Speed = 3;
+const Rotate_Speed = 0.03;
+
+// Animate Asteroids and spaceship
+function animate() {
+    window.requestAnimationFrame(animate);
+    c.fillStyle = "black";
+    c.fillRect(0, 0, canvas.width, canvas.height);
+
+        spaceship.update();
+
+        if (keys.w.pressed) {
+            
+            spaceship.velocity.x = Math.cos(spaceship.rotation) * Speed;
+            spaceship.velocity.y = Math.sin(spaceship.rotation) * Speed;
+        } 
+        else{
+            spaceship.velocity.x = 0;
+            spaceship.velocity.y = 0;
+        }
+
+        if(keys.a.pressed){
+            spaceship.rotation -= Rotate_Speed;
+        }
+
+        else if(keys.d.pressed){
+            spaceship.rotation += Rotate_Speed;
+        }
+
+    // Update and draw asteroids
+    for (let i = asteroids.length - 1; i >= 0; i--) {
+
+        const asteroid = asteroids[i];
+        asteroid.update();
+
+    // Remove asteroids that are off screen
+        if (
+            asteroid.position.x + asteroid.radius < 0 ||
+            asteroid.position.x - asteroid.radius > canvas.width ||
+            asteroid.position.y + asteroid.radius < 0 ||
+            asteroid.position.y - asteroid.radius > canvas.height) {
+                asteroids.splice(i, 1);
+            }
+    }
+}
+
+    // Movement keys
+    window.addEventListener("keydown", (event) => {
+        switch(event.code){
+            case "KeyW":
+                keys.w.pressed = true;
+                break;
+            case "KeyA":
+                keys.a.pressed = true;
+                break;  
+            case "KeyD":
+                keys.d.pressed = true;
+                break;
+        }
+    });
+
+        window.addEventListener("keyup", (event) => {
+        switch(event.code){
+            case "KeyW":
+                keys.w.pressed = false;
+                break;
+            case "KeyA":
+                keys.a.pressed = false;
+                break;  
+            case "KeyD":
+                keys.d.pressed = false;
+                break;
+        }
+    });
+
 const projectiles = [];
 const asteroids = [];
+
+
+animate();
 
 window.setInterval(() => {
 
@@ -70,7 +205,8 @@ window.setInterval(() => {
             vy = 1;
             break;
     }
-    asteroids.push(new Asteroid({position: {
+    asteroids.push(new Asteroid({
+        position: {
         x: x,
         y: y,
     },
@@ -86,25 +222,3 @@ window.setInterval(() => {
 
 }, 3000);
 
-function animate() {
-    window.requestAnimationFrame(animate);
-    c.fillStyle = "black";
-    c.fillRect(0, 0, canvas.width, canvas.height);
-
-    for (let i = asteroids.length - 1; i >= 0; i--) {
-
-        const asteroid = asteroids[i];
-        asteroid.update();
-
-    // Remove asteroids that are off screen
-        if (
-            asteroid.position.x + asteroid.radius < 0 ||
-            asteroid.position.x - asteroid.radius > canvas.width ||
-            asteroid.position.y + asteroid.radius < 0 ||
-            asteroid.position.y - asteroid.radius > canvas.height) {
-                asteroids.splice(i, 1);
-            }
-    }
-}
-
-animate();
